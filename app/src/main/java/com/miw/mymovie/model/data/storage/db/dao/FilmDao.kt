0f09c0bbc95f47e00adfcd1bf799453c83116783
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-05-07 19:37:49 +0200

    DAOs

--- app/src/main/java/com/miw/mymovie/model/data/storage/db/dao/FilmDao.kt
+++ app/src/main/java/com/miw/mymovie/model/data/storage/db/dao/FilmDao.kt
@@ -1,8 +1,25 @@
 package com.miw.mymovie.model.data.storage.db.dao
 
 import androidx.room.*
+import com.miw.mymovie.model.data.storage.db.entities.FilmEntity
+import com.miw.mymovie.model.data.storage.db.entities.FilmTable
+import com.miw.mymovie.model.data.storage.db.entities.UserEntity
+import com.miw.mymovie.model.data.storage.db.entities.UserTable
 
 @Dao
 interface FilmDao {
+    @Insert
+    fun insert (film: FilmEntity): Long
 
+    @Update
+    fun update (vararg film: FilmEntity)
+
+    @Delete
+    fun delete (film: FilmEntity)
+
+    @Query("DELETE FROM ${FilmTable.TABLE_NAME}")
+    fun clear()
+
+    @Query("SELECT * FROM ${FilmTable.TABLE_NAME} WHERE ${FilmTable.USERNAME} LIKE :username")
+    fun getAll(username: String): List<UserEntity>
 }
--- app/src/main/java/com/miw/mymovie/model/data/storage/db/dao/UserDao.kt
+++ app/src/main/java/com/miw/mymovie/model/data/storage/db/dao/UserDao.kt
@@ -1,7 +1,25 @@
 package com.miw.mymovie.model.data.storage.db.dao
 import androidx.room.*
+import com.miw.mymovie.model.User
+import com.miw.mymovie.model.data.storage.db.entities.UserEntity
+import com.miw.mymovie.model.data.storage.db.entities.UserTable
 
 @Dao
 interface UserDao {
+
+    @Insert
+    fun insert (user: UserEntity): Long
+
+    @Update
+    fun update (vararg user: UserEntity)
+
+    @Delete
+    fun delete (user: UserEntity)
+
+    @Query("DELETE FROM ${UserTable.TABLE_NAME}")
+    fun clear()
+
+    @Query("SELECT * FROM ${UserTable.TABLE_NAME}")
+    fun getAll(): List<UserEntity>
 
 }
